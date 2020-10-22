4230639dbe126505d3da69d80253f16e7e268e99
Tom Biato <tom.biato@ynov.com>
2020-10-22 14:30:41 +0200

    [TS] Add Todo interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import Title from "./components/Title";
 import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList"
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <Title title="Titre Super"/>
+      <Title title="Titre Super" />
       <TodoForm />
       <TodoList />
     </div>
